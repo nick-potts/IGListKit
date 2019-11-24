617b10eb62e4b7ce0ae1912ce3702cdd4d37d358
Nick <github@nickpotts.com.au>
2019-11-24 12:24:41 +1000

    Update SPM to include IGListKitDiff

--- Package.swift
+++ Package.swift
@@ -1,15 +1,24 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "IGListKit",
+    platforms: [
+      .macOS(.v10_11), .iOS(.v9), .tvOS(.v9)
+    ],
     products: [
-        .library(name: "IGListKit", targets: ["IGListKit"])
+        .library(name: "IGListKit", targets: ["IGListKit"]),
+        .library(name: "IGListDiffKit", targets: ["IGListDiffKit"])
+
     ],
     targets: [
         .target(
+            name: "IGListDiffKit",
+            path: "Source/IGListDiffKit"
+        ),
+        .target(
             name: "IGListKit",
-            path: "Source"
+            path: "Source/IGListKit"
         )
     ]
 )
